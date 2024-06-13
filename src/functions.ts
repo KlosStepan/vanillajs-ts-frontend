c8dec864284ef7107693bac7526e5a031bf33c1e
Stepan Klos <stepanklos@gmail.com>
2024-06-13 14:21:51 +0200

    Init code working w/ functions in separate file

---
+++ src/functions.ts
@@ -0,0 +1,11 @@
+const content = document.getElementById('content');
+
+function add(x: number, y: number): void {
+    const result: number = x + y;
+    if (content) { // Check if content is not null
+        content.innerHTML = `<span>${result}</span>`;
+    }
+}
+if (content) {
+    add(12, 17);
+}
